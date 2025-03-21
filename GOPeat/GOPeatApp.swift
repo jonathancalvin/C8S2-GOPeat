bf61547004f884f027816a11f1e6aad451e1d531
oxa <oxamarvel@gmail.com>
2025-03-21 11:17:43 +0700

    Initial Commit

---
+++ GOPeat/GOPeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GOPeatApp.swift
+//  GOPeat
+//
+//  Created by Oxa Marvel on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GOPeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
